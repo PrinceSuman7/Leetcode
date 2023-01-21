df882d6f17e0ddc79655611834e1cbadf6093179
iamprincesuman <iamprincesuman@gmail.com>
2023-01-21 14:54:05 +0530

    TwoSum.java

---
+++ java/two_sum.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], i);
+            }
+        }
+        int res[] = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i){
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return null;
+    }
+}
