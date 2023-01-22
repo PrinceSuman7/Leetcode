2bced1e299a8042223e889f5d41c0fbafd3a6cd6
iamprincesuman <iamprincesuman@gmail.com>
2023-01-22 22:49:08 +0530

    palindrome_partitioning.java

---
+++ java/Palindrome Partitioning.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        if(s.length() == 0) return new ArrayList<>();
+        helper(s, new ArrayList<String>(), res);
+        return res;
+    }
+    public void helper(String s, List<String> list, List<List<String>> res){
+        if(s.length() == 0 || s == null){
+            res.add(new ArrayList<String>(list));
+            return;
+        }
+        for(int i = 1; i <= s.length(); i++){
+            String temp = s.substring(0, i);
+            if(!isPalindrome(temp)) continue;
+            list.add(temp);
+            helper(s.substring(i, s.length()), list, res);
+            list.remove(list.size() - 1);
+        }
+        return;
+    }
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while(left <= right) {
+            if(s.charAt(left) != s.charAt(right))
+                return false;
+            left ++;
+            right --;
+        }
+        return true;
+    }
+}
--- java/two_sum.java
+++ java/two_sum.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             if(!map.containsKey(nums[i])){
@@ -14,5 +14,12 @@ class Solution {
             }
         }
         return null;
+    }
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        arr[0] = 1;
+        arr[1] = 3;
+        arr[2] = 1;
+        arr[3] = 2;
     }
 }
